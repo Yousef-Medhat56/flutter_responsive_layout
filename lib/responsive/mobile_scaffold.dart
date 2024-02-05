3ddd91c69d9690bc3230e52ed7599a2cf9873a6c
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-02-05 17:12:46 +0200

    feat: add mobile scaffold

---
+++ lib/responsive/mobile_scaffold.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/constants.dart';
+import 'package:responsive_design/utils/my_box.dart';
+import 'package:responsive_design/utils/my_tile.dart';
+
+class MobileScaffold extends StatefulWidget {
+  const MobileScaffold({super.key});
+
+  @override
+  State<MobileScaffold> createState() => _MobileScaffoldState();
+}
+
+class _MobileScaffoldState extends State<MobileScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar,
+      drawer: myDrawer,
+      backgroundColor: myDefaultBg,
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 12,
+          ),
+          AspectRatio(
+            aspectRatio: 1,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                  itemCount: 4,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return const MyBox();
+                  }),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const MyTile();
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
